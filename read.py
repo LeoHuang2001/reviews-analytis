42b6e8a7d6517b6c76334c6bc9541e1630ab6798
Leo Huang <huang900508@gmail.com>
2021-11-13 13:55:04 +0800

    留言平均長度

--- read.py
+++ read.py
@@ -6,3 +6,10 @@ with open('reviews.txt', 'r') as f:
 		count += 1
 		if count % 1000 == 0:
 			print(len(data))
+print('檔案讀取完了總, 總共有',len(data), '筆資料')
+
+sum_len = 0
+for x in data:
+	sum_len += len(x)
+averge = sum_len/len(data)
+print('平均長度是', averge)
